f06f32536725184303d22d9651e2d426eb41a588
Arhimeas <arhimeas@gmail.com>
2016-06-21 16:16:47 +0200

    2 examples de code Arduino Wifi
    Wifi_serv_test c'est pour connecter l'ESP à l'internet
    Wifi_web_page_test c'est pour mettre l'ESP en AP et lui faire créer une page internet

---
+++ Wifi_web_page_test.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiAPPSK[] = "Sylvain_Amica";
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+// const int LED_PIN = 5; // Thing's onboard, green LED
+// const int ANALOG_PIN = A0; // The only analog pin on the Thing
+// const int DIGITAL_PIN = 12; // Digital pin to be read
+
+WiFiServer server(80);
+
+void setup()
+{
+  initHardware();
+  setupWiFi();
+  server.begin();
+}
+
+void loop()
+{
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");
+  client.println();
+  // send web page
+  client.println("<!DOCTYPE html>");
+  client.println("<html>");
+  client.println("<head>");
+  client.println("<title>Arduino Web Page</title>");
+  client.println("</head>");
+  client.println("<body>");
+  client.println("<h1>Hello from Arduino!</h1>");
+  client.println("<p>A web page from the Arduino server</p>");
+  client.println("<button id=\"11\" class=\"led\">Toggle Pin 11</button>");
+  client.println("</body>");
+  client.println("</html>");
+
+  delay(1);
+  Serial.println("Client disonnected");
+
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
+}
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, 0, AP_NameString.length() + 1);
+
+  for (int i = 0; i < AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
+void initHardware()
+{
+  Serial.begin(115200);
+  // Don't need to set ANALOG_PIN as input,
+  // that's all it can be.
+}
