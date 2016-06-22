c5e3f65da8db5fb86a47bf8996b4bb1ed2a2b8f1
Arhimeas <arhimeas@gmail.com>
2016-06-22 12:38:06 +0200

    Suppression de commentaire

--- Wifi_web_page_test.ino
+++ Wifi_web_page_test.ino
@@ -3,7 +3,7 @@
 //////////////////////
 // WiFi Definitions //
 //////////////////////
-const char WiFiAPPSK[] = "Sylvain_Amica"; // Juste pour créer une nouvelle branche
+const char WiFiAPPSK[] = "Sylvain_Amica";
 
 /////////////////////
 // Pin Definitions //
