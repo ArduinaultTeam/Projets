b9462fe904761a126ef61e11aee140e88180a295
Arhimeas <arhimeas@gmail.com>
2017-01-06 11:57:08 +0100

    Mises à jour mineures du code
    Rajout de quelques commentaires
    Modification des conditions de ISR3

--- Indicateur_rapport_engage/sketch_IndRappEng_3.3.ino
+++ Indicateur_rapport_engage/sketch_IndRappEng_3.3.ino
@@ -1,3 +1,24 @@
+/* 
+Projet : Indicateur de rapport engagé
+Développeur : Super Benji
+
+But du projet : A l'aide de 2 contacteurs magnétiques on détecte le passage de rapport d'une moto
+Un reset est effectué à l'aide du voyant du neutre sur le tableau de bord
+
+Comment : 2 contacteurs magnétiques sont liés aux interruptions de la carte Arduino
+Le voyant est sur une entrée numérique
+Un afficheur 7 segments permet d'afficher le rapport engagé
+
+*/
+
+/***** Liste des bibliothèques *****/
+
+// Bibliothèque pour les interruptions
+  #include <PinChangeInt.h>
+
+
+/***** Variables globales *****/
+
 // Variables compteur et neutre.  
   int compteur = 0;
   int neutre = 0;
@@ -11,64 +32,68 @@
   const int F = 10;
   const int G = 11;
 
-// Interruption:
-  #include <PinChangeInt.h>
 
-// Signal du neutre: si neutre<=1 -> neutre=0 & compteur=0.  
+/***** Fonctions d'interruptions *****/
+
+// Signal du neutre: neutre=0 && compteur=0.  
   void ISR3()
-{
-  if (neutre <= 1)
   {
     neutre = 0;
     compteur = 0;
-  }
   }
      
 // Contacteur haut: si neutre>=0 & compteur<=1 -> Compteur=2, Sinon incremente compteur sauf si compteur=5.
   void ISR1()
   {
-    if (neutre >= 0 && compteur <= 1)
+  if (neutre >= 0 && compteur <= 1)
   {
-  compteur = 2;
-  neutre = 1;
-}
+    compteur = 2;
+    neutre = 1;
+  }
 
-else if (compteur == 5)
-{
+  else if (compteur == 5)
+  {
   }
   
-else 
-    {
-      compteur ++;
-      neutre = 1;
-       } 
-}
+  else 
+  {
+    compteur ++;
+    neutre = 1;
+  } 
+  }
 
 // Contacteur bas: si neutre=0 & compteur=0 -> Compteur=1, Sinon decremente compteur sauf si compteur=1.
   void ISR2()
-   {
-    if (neutre == 0 && compteur == 0) 
-    
-    {
-      compteur = 1; neutre = 1;
-       }
-else if (compteur == 1)
-{
+  {
+  if (neutre == 0 && compteur == 0) 
+  {
+    compteur = 1; neutre = 1;
+  }
+  
+  else if (compteur == 1)
+  {
   }
          
- else 
-    {
-      compteur --;
-      neutre = 1;
-       }    
+  else 
+  {
+    compteur --;
+    neutre = 1;
+  }    
   }
+
+
+/***** Fonction setup de l'Arduino *****/
+
 void setup() {
-inithardware();  
+  inithardware();  
 
   PCintPort::attachInterrupt (2,ISR1,RISING);
   PCintPort::attachInterrupt (3,ISR2,RISING);
   PCintPort::attachInterrupt (4,ISR3,RISING);
 }
+
+
+/***** Fonction loop de l'Arduino *****/
 
 void loop() {
  
