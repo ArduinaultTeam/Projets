7cbe937ebdab8fe976c452765991fd4efce06a6f
Arhimeas <arhimeas@gmail.com>
2017-07-20 15:48:48 +0200

    Indications pour la suite du projet

--- Truck_CANOpen/CAN.ino
+++ Truck_CANOpen/CAN.ino
@@ -1,17 +1,29 @@
-# define SOF
+// Les defines suivants sont à redéfinir
 # define arbitrage 0
 # define commande 0
 # define donnees 0
-# define CRC 0
-# define ACK 0
-# define EOF 0
+
+struct message envoie = {arbitrage, commande, donnees};
+
+// Le code suivant ne marche pas !
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+byte envoie.DLC = 0b000010;
 
+bool ext = envoie.DLC>>5;
+
+int mask = 0b001111;
+
+int len = mask & envoie.DLC; // 0b000010 = 2
+
+CAN.sendMsgbuf(envoie.arbitrage,ext,len,envoie.donnees);
+
+envoie.donnees++;
+
+delay(100);
 }
