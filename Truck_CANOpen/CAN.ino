5845fb91d5a677866f4c1bf180726ee225aaa2da
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-07-20 14:40:16 +0200

    Premier envoi

---
+++ Truck_CANOpen/CAN.ino
@@ -0,0 +1,17 @@
+# define SOF
+# define arbitrage 0
+# define commande 0
+# define donnees 0
+# define CRC 0
+# define ACK 0
+# define EOF 0
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Truck_CANOpen/struct_message.ino
@@ -0,0 +1,12 @@
+struct message {
+  byte SOF;
+  short int arbitrage;
+  char commande;
+  double donnees;
+  short int CRC;
+  byte ACK:2;
+  char EOF;
+};
+
+struct message createMessage
+
