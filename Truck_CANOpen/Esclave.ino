65f78b5e298fe956f6583cf16553d54d056568d6
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-08-24 08:42:57 +0200

    Dernière MAJ
    Programme ok
    Attention quelques bugs sur les leds

--- Truck_CANOpen/Esclave.ino
+++ Truck_CANOpen/Esclave.ino
@@ -9,12 +9,16 @@
 MCP_CAN CAN(SPI_CS_PIN);
 
 // Affectation
+#define BLANC 3
 #define ROUGE 4
+
+// Caractéristiques des messages
 #define LONGUEUR_DATA 8
 #define LENGTH 8
 
 // Déclaration variable
 int led_rouge = 0;
+int led_blanche = 0;
 
 /**************************************************************************************/
 /*                                INITIALISATION                                      */
@@ -31,9 +35,11 @@ void setup() {
   } // Fin while
   
   Serial.println("CAN BUS Shield init ok!");
-
-  pinMode(ROUGE, OUTPUT);
-  digitalWrite(ROUGE, LOW);        
+  for(int i = 3; i < 5; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW); 
+  }
+       
 } // Fin setup
 
 unsigned char stmp[LONGUEUR_DATA] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -67,6 +73,17 @@ void loop() {
     
     Serial.println("");
     } // Fin if
+
+    if(buf[0] == 10) {
+      led_blanche = buf[4];
+      if (led_blanche == 1) digitalWrite(BLANC, HIGH);
+      else digitalWrite(BLANC, LOW);
+
+      // Acknowledgment
+      stmp[0] = 11;
+      CAN.sendMsgBuf(0x02, 0, LENGTH, stmp);
+      stmp[0] = 0;      
+    }
     
     if(buf[0] == 100) {
       led_rouge = buf[5];
@@ -76,10 +93,10 @@ void loop() {
       // Acknowledgment
       stmp[0] = 101;
       CAN.sendMsgBuf(0x02, 0, LENGTH, stmp);
-      delay(100);
       stmp[0] = 0;
     } // Fin if
-  } // Fin CAN receive 
+  } // Fin CAN receive
+  delay(100);
 } // Fin loop
 
 // --- Fin du programme ---
