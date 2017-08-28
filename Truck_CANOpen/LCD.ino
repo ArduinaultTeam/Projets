75892e51096c76f33a2e5a175efa8aaed77cd048
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-08-28 16:26:04 +0200

    Test ack
    bug

--- Truck_CANOpen/LCD.ino
+++ Truck_CANOpen/LCD.ino
@@ -8,7 +8,7 @@
 #define SPI_CS_PIN 9
 MCP_CAN CAN(SPI_CS_PIN);
 
-// Libraire Ecran LCD
+// Librairie Ecran LCD
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 7, 6, 5, 4, 3);
@@ -17,14 +17,20 @@ LiquidCrystal lcd(8, 7, 6, 5, 4, 3);
 #define LONGUEUR_DATA 8
 #define LENGTH 8
 
-// Déclaration variable
+// Déclaration variables
 int valeur_lum = 0;
 int valeur_tmp = 0;
 int tmp = 0;
 
-// Déclaration temporisation
+// Déclaration temporisations
 unsigned long int temps_cycle = 100;
 unsigned long int t = 0;
+
+// Détection des erreurs
+int nombre_erreurs_max = 10;
+int compteur_erreur_led_blanche = 0;
+
+bool message_pret = false;
 
 /**************************************************************************************/
 /*                                INITIALISATION                                      */
@@ -42,8 +48,7 @@ void setup() {
   
   Serial.println("CAN BUS Shield init ok!");
 
-  // Init ecran LCD
-  lcd.begin(16,2);
+  lcd.begin(16,2); // Init ecran LCD
 } // Fin setup
 
 unsigned char stmp[LONGUEUR_DATA] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -57,6 +62,35 @@ void loop() {
 
   unsigned char len = 0;
 
+  // Code jour/nuit **********
+  if(millis() - t > temps_cycle) {
+    stmp[0] = 3;
+    if(valeur_lum < 800) {
+      stmp[4] = 1;
+      t = millis();
+      compteur_erreur_led_blanche++;
+    } // Fin if
+    else {
+      stmp[4] = 0;
+      t = millis();
+      compteur_erreur_led_blanche++;
+    } // Fin else
+    CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);    
+  } // Fin if code jour/nuit
+
+
+  // Code erreur **********
+  else if (compteur_erreur_led_blanche >= nombre_erreurs_max) {
+    stmp[0] = 102; // Ce code serait donc une erreur de communication
+
+    if(compteur_erreur_led_blanche > nombre_erreurs_max) stmp[4] = 1; // Erreur communication lumière
+    else stmp[4] = 0;
+
+    CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);
+  } // Fin else if code erreur
+  Serial.println(compteur_erreur_potar);
+    
+  
   // Check if data coming
   if(CAN_MSGAVAIL == CAN.checkReceive()) {
     // Read data,  len: data length, buf: data buf
@@ -78,49 +112,44 @@ void loop() {
     Serial.println("");
     } // Fin if
 
-    if(buf[0] == 2) { // Réception infos des capteurs
-      // Code Lumière
+ 
+    // Code réception infos des capteurs  **********
+    if(buf[0] == 2) {
+      // Code Lumière **********
       valeur_lum = map(buf[7], 0, 255, 0, 1023);
-      Serial.println(valeur_lum);
     
-      // Code Température
+      // Code Température **********
       valeur_tmp = map(buf[6], 0, 255, 0, 1750);
       tmp = map(valeur_tmp, 0, 1750, -50, 125);
-      Serial.println(tmp);
   
-      // Code Ecran LCD
+      // Code Ecran LCD **********
       lcd.setCursor(0,0);  // Curseur à gauche sur la première ligne
-      if(millis() - t > temps_cycle) {
-        if(valeur_lum < 750) {
-          lcd.print("Nuit");
-          stmp[4] = 1;
-          t = millis();
-        }
-      
-        else {
-          lcd.print("Jour");
-          stmp[4] = 0;
-          t = millis();
-        }
-      }  
-      
-      lcd.setCursor(0,1);  // Curseur à gauche sur la deuxième ligne
+      if(valeur_lum < 800) lcd.print("Nuit");   
+      else lcd.print("Jour");
+      lcd.setCursor(0,1); // Curseur à gauche sur la deuxième ligne
       lcd.print(valeur_lum);
-      lcd.setCursor(9,0);  // Curseur à la neuvième case de la première ligne
+      lcd.setCursor(9,0); // Curseur à la neuvième case de la première ligne
       lcd.print("Tmp :");
-      lcd.setCursor(9,1);  // Curseur à la neuvième case de la seconde ligne
+      lcd.setCursor(9,1); // Curseur à la neuvième case de la seconde ligne
       lcd.print(tmp);
+      stmp[0] = 2;
 
-      // Acknowledgment
-      for(int i =0; i< LONGUEUR_DATA;i++)
+      // Acknowledgment **********
+      for(int i =0; i< LONGUEUR_DATA;i++)  {
         stmp[i] = buf[i];
+      } // Fin for
       stmp[1] = 1;
-      
       CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);
       stmp[0] = 0;
-      
     } // Fin if
   } // Fin CAN receive
+
+  // Traitement des acknowledgment **********
+  if(buf[0] == 3 && buf[1] == 1) { // Dès que l'on reçoit l'acknowledgment capteurs de la part du LCD on peut reset les compteurs d'erreur
+    compteur_erreur_led_blanche = 0;
+  } // Fin if buf[0] = 2
+  // Fin du traitement des acknowledgement
+  
   delay(100);
 } // Fin loop
 
