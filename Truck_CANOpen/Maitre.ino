41939df8b4e5d3ff918a56967f3c552e9a5ca69a
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-08-28 17:48:35 +0200

    Update Maître
    Programme OK

--- Truck_CANOpen/Maitre.ino
+++ Truck_CANOpen/Maitre.ino
@@ -32,13 +32,13 @@ int old_bouton = 0;
 int valeur_potar = 0;
 
 // Déclaration temporisations
-unsigned long int temps_cycle1 = 1000; // Lumière
+unsigned long int temps_cycle1 = 1200; // Lumière
 unsigned long int t1 = 0;
-unsigned long int temps_cycle2 = 10000; // Température
+unsigned long int temps_cycle2 = 12000; // Température
 unsigned long int t2 = 0;
-unsigned long int temps_cycle3 = 100; // Bouton
+unsigned long int temps_cycle3 = 125; // Bouton
 unsigned long int t3 = 0;
-unsigned long int temps_cycle4 = 100; // Potar
+unsigned long int temps_cycle4 = 37; // Potar
 unsigned long int t4 = 0;
 
 // Détection des erreurs
@@ -78,90 +78,8 @@ unsigned char buf[LONGUEUR_DATA];
 
 void loop() {
 
-  if((compteur_erreur_lum < nombre_erreurs_max) || (compteur_erreur_temp < nombre_erreurs_max) || (compteur_erreur_bouton < nombre_erreurs_max) || (compteur_erreur_potar < nombre_erreurs_max)) // Si cette condition est respectée tout va bien
-  {
-    // Code lumière **********
-    if(millis() - t1 > temps_cycle1) {
-      valeur_lum = map(analogRead(LUM), 0, 1023, 0, 255);  // Transforme la valeur pour le CAN
-      stmp[0] = 2;
-      stmp[7] = valeur_lum;
-      t1 = millis();    
-      message_pret = true;
-      compteur_erreur_lum++;
-    } // Fin if lumière
-      
-    
-    // Code Température **********
-    if((millis() - t2 > temps_cycle2) || premier_cycle == 1) {
-      valeur_tension = map(analogRead(TMP), 0, 1023, 0, 5000);  // Transforme la valeur lue en tension
-      valeur_tmp = map(valeur_tension, 0, 1750, 0, 255);  // Transforme la tension pour le CAN
-      stmp[0] = 2; //
-      stmp[6] = valeur_tmp;
-      t2 = millis();
-      message_pret = true;
-      premier_cycle = 0;
-      compteur_erreur_temp++;
-    } // Fin if température
-  
-  
-    // Code interrupteur **********
-    if(digitalRead(BOUTON) && (old_bouton==0) && (millis() - t3 > temps_cycle3)) { // On lit l'état du bouton et on teste si le bouton est pressé
-      interrupteur = !interrupteur; // Si oui on change l'état de l'interrupteur
-      stmp[0] = 100;
-      stmp[5] = interrupteur;
-      t3 = millis();
-      message_pret = true;
-      compteur_erreur_bouton++;
-    } // Fin if interrupteur
-    old_bouton = digitalRead(BOUTON);
-    
-    
-    // Code potar **********
-    if(interrupteur && (millis() - t4 > temps_cycle4) && (millis() - t3 > temps_cycle4)) {
-      valeur_potar = map(analogRead(POTAR), 0, 1023, 0, 255);
-      stmp[0] = 20;
-      stmp[1] = 0;
-      stmp[3] = valeur_potar; 
-      t4 = millis();
-      message_pret = true;
-      compteur_erreur_potar++;
-    } // Fin if potar
-  } // Fin if erreurs compteur
+    unsigned char len = 0;
 
-  
-  // Code erreur **********
-  else if ((compteur_erreur_temp >= nombre_erreurs_max) || (compteur_erreur_lum >= nombre_erreurs_max) || (compteur_erreur_bouton >= nombre_erreurs_max) || (compteur_erreur_potar >= nombre_erreurs_max)) {
-    stmp[0] = 102; // Ce code serait donc une erreur de communication
-
-    if(compteur_erreur_lum > nombre_erreurs_max) stmp[7] = 1; // Erreur communication lumière
-    else stmp[7] = 0;
-    
-    if(compteur_erreur_temp > nombre_erreurs_max) stmp[6] = 1; // Erreur communication température
-    else stmp[6] = 0;
-
-    if(compteur_erreur_bouton > nombre_erreurs_max) stmp[5] = 1; // Erreur communication température
-    else stmp[5] = 0;
-
-    if(compteur_erreur_potar > nombre_erreurs_max) stmp[3] = 1; // Erreur communication température
-    else stmp[3] = 0;
-    
-    CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
-  } // Fin else if
-  Serial.println(compteur_erreur_lum);
-  Serial.println(compteur_erreur_temp);
-  Serial.println(compteur_erreur_bouton);
-  Serial.println(compteur_erreur_potar);
-
-  // Code envoyer message **********
-  if(message_pret) {
-      CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
-      message_pret = false;
-    } // Fin if message_pret
-
-
-  unsigned char len = 0;
-
-  
   // Check if data coming **********
   if(CAN_MSGAVAIL == CAN.checkReceive()) {
     // Read data,  len: data length, buf: data buf
@@ -200,7 +118,86 @@ void loop() {
   // Fin du traitement des acknowledgement
 
   
-  delay(100);
+  if((compteur_erreur_lum < nombre_erreurs_max) || (compteur_erreur_temp < nombre_erreurs_max) || (compteur_erreur_bouton < nombre_erreurs_max) || (compteur_erreur_potar < nombre_erreurs_max)) // Si cette condition est respectée tout va bien
+  {
+    // Code lumière **********
+    if(millis() - t1 > temps_cycle1) {
+      valeur_lum = map(analogRead(LUM), 0, 1023, 0, 255);  // Transforme la valeur pour le CAN
+      stmp[0] = 2;
+      stmp[7] = valeur_lum;
+      t1 = millis();    
+      message_pret = true;
+      compteur_erreur_lum++;
+    } // Fin if lumière
+      
+    
+    // Code température **********
+    if((millis() - t2 > temps_cycle2) || premier_cycle == 1) {
+      valeur_tension = map(analogRead(TMP), 0, 1023, 0, 5000);  // Transforme la valeur lue en tension
+      valeur_tmp = map(valeur_tension, 0, 1750, 0, 255);  // Transforme la tension pour le CAN
+      stmp[0] = 2; //
+      stmp[6] = valeur_tmp;
+      t2 = millis();
+      message_pret = true;
+      premier_cycle = 0;
+      compteur_erreur_temp++;
+    } // Fin if température
+  
+  
+    // Code interrupteur **********
+    if(digitalRead(BOUTON) && (old_bouton==0) && (millis() - t3 > temps_cycle3)) { // On lit l'état du bouton et on teste si le bouton est pressé
+      interrupteur = !interrupteur; // Si oui on change l'état de l'interrupteur
+      stmp[0] = 100;
+      stmp[5] = interrupteur;
+      t3 = millis();
+      message_pret = true;
+      compteur_erreur_bouton++;
+    } // Fin if interrupteur
+    old_bouton = digitalRead(BOUTON);
+    
+    
+    // Code potar **********
+    if(interrupteur && (millis() - t4 > temps_cycle4) && (millis() - t3 > temps_cycle4)) {
+      valeur_potar = map(analogRead(POTAR), 0, 1023, 0, 255);
+      stmp[0] = 20;
+      stmp[1] = 0;
+      stmp[3] = valeur_potar; 
+      t4 = millis();
+      message_pret = true;
+      compteur_erreur_potar++;
+    } // Fin if potar
+  } // Fin if erreurs compteur
+  
+  
+  // Code envoyer message **********
+  if(message_pret) {
+      CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
+      message_pret = false;
+    } // Fin if message_pret
+   
+  
+  // Code erreur **********
+  if ((compteur_erreur_temp >= nombre_erreurs_max) || (compteur_erreur_lum >= nombre_erreurs_max) || (compteur_erreur_bouton >= nombre_erreurs_max) || (compteur_erreur_potar >= nombre_erreurs_max)) {
+    stmp[0] = 102; // Ce code serait donc une erreur de communication
+
+    if(compteur_erreur_lum > nombre_erreurs_max) stmp[7] = 1; // Erreur communication lumière
+    else stmp[7] = 0;
+    
+    if(compteur_erreur_temp > nombre_erreurs_max) stmp[6] = 1; // Erreur communication température
+    else stmp[6] = 0;
+
+    if(compteur_erreur_bouton > nombre_erreurs_max) stmp[5] = 1; // Erreur communication température
+    else stmp[5] = 0;
+
+    if(compteur_erreur_potar > nombre_erreurs_max) stmp[3] = 1; // Erreur communication température
+    else stmp[3] = 0;
+    
+    delay(100);
+    Serial.println("errreur");
+    CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
+  } // Fin else if
+  
+  delay(10);
 }  // Fin loop
 
 // --- Fin du programme ---
