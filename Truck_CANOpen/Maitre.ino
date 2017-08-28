28b7d957f73b83d9d362d30490b35a4453e602d8
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-08-28 16:25:37 +0200

    Test ack
    Bug

--- Truck_CANOpen/Maitre.ino
+++ Truck_CANOpen/Maitre.ino
@@ -8,7 +8,7 @@
 #define SPI_CS_PIN 9
 MCP_CAN CAN(SPI_CS_PIN);
 
-// Affectation
+// Affectations
 #define LUM A0
 #define TMP A1
 #define POTAR A2
@@ -18,7 +18,7 @@ MCP_CAN CAN(SPI_CS_PIN);
 #define LONGUEUR_DATA 8
 #define LENGTH 8
 
-// Déclaration variable
+// Déclaration variables
 int valeur_lum = 0;
 
 int premier_cycle = 1;
@@ -31,7 +31,7 @@ int old_bouton = 0;
 
 int valeur_potar = 0;
 
-// Déclaration temporisation
+// Déclaration temporisations
 unsigned long int temps_cycle1 = 1000; // Lumière
 unsigned long int t1 = 0;
 unsigned long int temps_cycle2 = 10000; // Température
@@ -41,10 +41,12 @@ unsigned long int t3 = 0;
 unsigned long int temps_cycle4 = 100; // Potar
 unsigned long int t4 = 0;
 
-// Detection des erreurs
+// Détection des erreurs
 int nombre_erreurs_max = 10;
 int compteur_erreur_lum = 0;
 int compteur_erreur_temp = 0;
+int compteur_erreur_bouton = 0;
+int compteur_erreur_potar = 0;
 
 bool message_pret = false;
 
@@ -76,39 +78,59 @@ unsigned char buf[LONGUEUR_DATA];
 
 void loop() {
 
-  if((compteur_erreur_lum < nombre_erreurs_max) || (compteur_erreur_temp < nombre_erreurs_max)) // Si cette condition est respectée tout va bien
+  if((compteur_erreur_lum < nombre_erreurs_max) || (compteur_erreur_temp < nombre_erreurs_max) || (compteur_erreur_bouton < nombre_erreurs_max) || (compteur_erreur_potar < nombre_erreurs_max)) // Si cette condition est respectée tout va bien
   {
-    // Code lumière
+    // Code lumière **********
     if(millis() - t1 > temps_cycle1) {
       valeur_lum = map(analogRead(LUM), 0, 1023, 0, 255);  // Transforme la valeur pour le CAN
       stmp[0] = 2;
       stmp[7] = valeur_lum;
-      t1 = millis();
-      
+      t1 = millis();    
       message_pret = true;
-      
       compteur_erreur_lum++;
     } // Fin if lumière
       
     
-    // Code Température
-    if(millis() - t2 > temps_cycle2 || premier_cycle == 1) {
+    // Code Température **********
+    if((millis() - t2 > temps_cycle2) || premier_cycle == 1) {
       valeur_tension = map(analogRead(TMP), 0, 1023, 0, 5000);  // Transforme la valeur lue en tension
       valeur_tmp = map(valeur_tension, 0, 1750, 0, 255);  // Transforme la tension pour le CAN
       stmp[0] = 2; //
       stmp[6] = valeur_tmp;
       t2 = millis();
-
       message_pret = true;
-      
       premier_cycle = 0;
-      
       compteur_erreur_temp++;
     } // Fin if température
+  
+  
+    // Code interrupteur **********
+    if(digitalRead(BOUTON) && (old_bouton==0) && (millis() - t3 > temps_cycle3)) { // On lit l'état du bouton et on teste si le bouton est pressé
+      interrupteur = !interrupteur; // Si oui on change l'état de l'interrupteur
+      stmp[0] = 100;
+      stmp[5] = interrupteur;
+      t3 = millis();
+      message_pret = true;
+      compteur_erreur_bouton++;
+    } // Fin if interrupteur
+    old_bouton = digitalRead(BOUTON);
+    
+    
+    // Code potar **********
+    if(interrupteur && (millis() - t4 > temps_cycle4) && (millis() - t3 > temps_cycle4)) {
+      valeur_potar = map(analogRead(POTAR), 0, 1023, 0, 255);
+      stmp[0] = 20;
+      stmp[1] = 0;
+      stmp[3] = valeur_potar; 
+      t4 = millis();
+      message_pret = true;
+      compteur_erreur_potar++;
+    } // Fin if potar
   } // Fin if erreurs compteur
 
-
-  else if ((compteur_erreur_temp >= nombre_erreurs_max) || (compteur_erreur_lum >= nombre_erreurs_max)) {
+  
+  // Code erreur **********
+  else if ((compteur_erreur_temp >= nombre_erreurs_max) || (compteur_erreur_lum >= nombre_erreurs_max) || (compteur_erreur_bouton >= nombre_erreurs_max) || (compteur_erreur_potar >= nombre_erreurs_max)) {
     stmp[0] = 102; // Ce code serait donc une erreur de communication
 
     if(compteur_erreur_lum > nombre_erreurs_max) stmp[7] = 1; // Erreur communication lumière
@@ -116,42 +138,31 @@ void loop() {
     
     if(compteur_erreur_temp > nombre_erreurs_max) stmp[6] = 1; // Erreur communication température
     else stmp[6] = 0;
+
+    if(compteur_erreur_bouton > nombre_erreurs_max) stmp[5] = 1; // Erreur communication température
+    else stmp[5] = 0;
+
+    if(compteur_erreur_potar > nombre_erreurs_max) stmp[3] = 1; // Erreur communication température
+    else stmp[3] = 0;
     
     CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
   } // Fin else if
+  Serial.println(compteur_erreur_lum);
+  Serial.println(compteur_erreur_temp);
+  Serial.println(compteur_erreur_bouton);
+  Serial.println(compteur_erreur_potar);
 
+  // Code envoyer message **********
   if(message_pret) {
       CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
       message_pret = false;
     } // Fin if message_pret
-    
-  // Code interrupteur
-  if(digitalRead(BOUTON) && (old_bouton==0) && (millis() - t3 > temps_cycle3)) { // On lit l'état du bouton et on teste si le bouton est pressé
-    
-      interrupteur = !interrupteur; // Si oui on change l'état de l'interrupteur
-      stmp[0] = 100;
-      stmp[5] = interrupteur;
-      t3 = millis();
-      CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);
-    
-  } // Fin if  
-  old_bouton = digitalRead(BOUTON);
-    
-
-  // Code potar
-  if(interrupteur && (millis() - t4 > temps_cycle4) && (millis() - t3 > temps_cycle4) ) {
-    valeur_potar = map(analogRead(POTAR), 0, 1023, 0, 255);
-    stmp[0] = 20;
-    stmp[1] = 0;
-    stmp[3] = valeur_potar; 
-    t4 = millis();
-    CAN.sendMsgBuf(0x01, 0, LENGTH, stmp);    
-  } // Fin if potar
 
 
   unsigned char len = 0;
+
   
-  // Check if data coming
+  // Check if data coming **********
   if(CAN_MSGAVAIL == CAN.checkReceive()) {
     // Read data,  len: data length, buf: data buf
     CAN.readMsgBuf(&len, buf);
@@ -168,25 +179,28 @@ void loop() {
         Serial.print(buf[i], DEC);
         Serial.print("\t");
       } // Fin for
-    
     Serial.println("");
-    
     } // Fin if
   } // Fin CAN receive
   
-  delay(100);
 
-
-  // Traitement des acknowledgment
-  if(buf[0] == 3) { // Dès que l'on reçoit l'acknowledgment capteurs de la part du LCD on peut reset les compteurs d'erreur
+  // Traitement des acknowledgment **********
+  if(buf[0] == 2 && buf[1] == 1) { // Dès que l'on reçoit l'acknowledgment capteurs de la part du LCD on peut reset les compteurs d'erreur
     compteur_erreur_lum = 0;
     compteur_erreur_temp = 0;
-  }  // Fin if traitement des acknowledgement
+  }  // Fin if buf[0] = 2
+
+  if(buf[0] == 20 && buf[1] == 1) { // Dès que l'on reçoit l'acknowledgment potar de la part de l'esclave on peut reset les compteurs d'erreur
+    compteur_erreur_potar = 0;
+  }  // Fin if buf[0] = 20
+
+  if(buf[0] == 100 && buf[1] == 1) { // Dès que l'on reçoit l'acknowledgment potar de la part de l'esclave on peut reset les compteurs d'erreur
+    compteur_erreur_bouton = 0;
+  }  // Fin if buf[0] = 100
+  // Fin du traitement des acknowledgement
+
   
-  if(buf[0] == 20 && buf[1] == 1) { 
-    
-  }  // Fin if traitement des acknowledgement
-  
+  delay(100);
 }  // Fin loop
 
 // --- Fin du programme ---
