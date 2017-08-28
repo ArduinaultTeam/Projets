ba2147a57af501f31cc63f083a3ff810ef5de27c
AnthoTruck <30285625+AnthoTruck@users.noreply.github.com>
2017-08-28 17:50:23 +0200

    Update LCD
    Programme OK

--- Truck_CANOpen/LCD.ino
+++ Truck_CANOpen/LCD.ino
@@ -22,6 +22,9 @@ int valeur_lum = 0;
 int valeur_tmp = 0;
 int tmp = 0;
 
+int flag = 0;
+int old_flag = 0;
+
 // Déclaration temporisations
 unsigned long int temps_cycle = 100;
 unsigned long int t = 0;
@@ -29,8 +32,6 @@ unsigned long int t = 0;
 // Détection des erreurs
 int nombre_erreurs_max = 10;
 int compteur_erreur_led_blanche = 0;
-
-bool message_pret = false;
 
 /**************************************************************************************/
 /*                                INITIALISATION                                      */
@@ -61,6 +62,80 @@ unsigned char buf[LONGUEUR_DATA];
 void loop() {
 
   unsigned char len = 0;
+  
+  // Check if data coming
+  if(CAN_MSGAVAIL == CAN.checkReceive()) {
+    // Read data,  len: data length, buf: data buf
+    CAN.readMsgBuf(&len, buf);
+
+    unsigned int canId = CAN.getCanId();
+    
+    if (buf[0] != 0) {
+      Serial.println("-----------------------------");
+      Serial.print("Get data from ID: ");
+      Serial.println(canId, DEC);
+      
+      // Print the data
+      for(int i = 0; i<len; i++) {
+        Serial.print(buf[i], DEC);
+        Serial.print("\t");
+      } // Fin for
+    
+    Serial.println("");
+    } // Fin if
+
+ 
+    // Code réception infos des capteurs  **********
+    if(buf[0] == 2) {
+      
+      // Code Lumière **********
+      valeur_lum = map(buf[7], 0, 255, 0, 1023);
+    
+      // Code Température **********
+      valeur_tmp = map(buf[6], 0, 255, 0, 1750);
+      tmp = map(valeur_tmp, 0, 1750, -50, 125);
+
+      // Code Ecran LCD **********
+      lcd.setCursor(0,0);  // Curseur à gauche sur la première ligne
+      if(valeur_lum < 800) lcd.print("Nuit");   
+      else lcd.print("Jour");
+      
+      if(valeur_lum > 1000) flag = 1;
+      else flag = 0;
+      if( !flag && old_flag) {
+        lcd.setCursor(4,1);
+        lcd.print(" ");
+      }
+      old_flag = flag;
+      
+      lcd.setCursor(0,1); // Curseur à gauche sur la deuxième ligne
+      lcd.print(valeur_lum);
+      
+      lcd.setCursor(9,0); // Curseur à la neuvième case de la première ligne
+      lcd.print("Tmp :");
+      
+      lcd.setCursor(9,1); // Curseur à la neuvième case de la seconde ligne
+      lcd.print(tmp);
+      
+      stmp[0] = 2;
+
+      // Acknowledgment **********
+      for(int i =0; i< LONGUEUR_DATA;i++)  {
+        stmp[i] = buf[i];
+      } // Fin for
+      stmp[1] = 1;
+      CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);
+      Serial.println("1");
+    } // Fin if
+  } // Fin CAN receive
+
+
+  // Traitement des acknowledgment **********
+  if(buf[0] == 3 && buf[1] == 1) {
+    compteur_erreur_led_blanche = 0;
+  } // Fin if buf[0] = 2
+  // Fin du traitement des acknowledgement
+
 
   // Code jour/nuit **********
   if(millis() - t > temps_cycle) {
@@ -88,69 +163,8 @@ void loop() {
 
     CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);
   } // Fin else if code erreur
-  Serial.println(compteur_erreur_potar);
-    
   
-  // Check if data coming
-  if(CAN_MSGAVAIL == CAN.checkReceive()) {
-    // Read data,  len: data length, buf: data buf
-    CAN.readMsgBuf(&len, buf);
-
-    unsigned int canId = CAN.getCanId();
-    
-    if (buf[0] != 0) {
-      Serial.println("-----------------------------");
-      Serial.print("Get data from ID: ");
-      Serial.println(canId, DEC);
-      
-      // Print the data
-      for(int i = 0; i<len; i++) {
-        Serial.print(buf[i], DEC);
-        Serial.print("\t");
-      } // Fin for
-    
-    Serial.println("");
-    } // Fin if
-
- 
-    // Code réception infos des capteurs  **********
-    if(buf[0] == 2) {
-      // Code Lumière **********
-      valeur_lum = map(buf[7], 0, 255, 0, 1023);
-    
-      // Code Température **********
-      valeur_tmp = map(buf[6], 0, 255, 0, 1750);
-      tmp = map(valeur_tmp, 0, 1750, -50, 125);
-  
-      // Code Ecran LCD **********
-      lcd.setCursor(0,0);  // Curseur à gauche sur la première ligne
-      if(valeur_lum < 800) lcd.print("Nuit");   
-      else lcd.print("Jour");
-      lcd.setCursor(0,1); // Curseur à gauche sur la deuxième ligne
-      lcd.print(valeur_lum);
-      lcd.setCursor(9,0); // Curseur à la neuvième case de la première ligne
-      lcd.print("Tmp :");
-      lcd.setCursor(9,1); // Curseur à la neuvième case de la seconde ligne
-      lcd.print(tmp);
-      stmp[0] = 2;
-
-      // Acknowledgment **********
-      for(int i =0; i< LONGUEUR_DATA;i++)  {
-        stmp[i] = buf[i];
-      } // Fin for
-      stmp[1] = 1;
-      CAN.sendMsgBuf(0x03, 0, LENGTH, stmp);
-      stmp[0] = 0;
-    } // Fin if
-  } // Fin CAN receive
-
-  // Traitement des acknowledgment **********
-  if(buf[0] == 3 && buf[1] == 1) { // Dès que l'on reçoit l'acknowledgment capteurs de la part du LCD on peut reset les compteurs d'erreur
-    compteur_erreur_led_blanche = 0;
-  } // Fin if buf[0] = 2
-  // Fin du traitement des acknowledgement
-  
-  delay(100);
+  delay(10);
 } // Fin loop
 
 // --- Fin du programme ---
